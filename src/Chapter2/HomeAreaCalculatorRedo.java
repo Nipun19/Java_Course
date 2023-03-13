bbb3a5bb8d86816cf7448f83f67cbd52eaecbf21
Nipun19 <Nizzasat1998@gmail.com>
2023-03-13 07:58:56 +0530

    new commit

---
+++ src/Chapter2/HomeAreaCalculatorRedo.java
@@ -0,0 +1,30 @@
+package Chapter2;
+
+import java.util.Scanner;
+
+public class HomeAreaCalculatorRedo {
+
+    public static void main(String[] args) {
+
+        HomeAreaCalculatorRedo calculator = new HomeAreaCalculatorRedo();
+        Rectangle kitchen = calculator.getRoom();
+        Rectangle bathroom = calculator.getRoom();
+
+        double area = calculator.calculateTotalArea(kitchen, bathroom);
+
+    }
+
+    public double calculateTotalArea(Rectangle rectangle1, Rectangle rectangle2) {
+        return rectangle1.calculateArea() + rectangle2.calculateArea();
+    }
+
+    public Rectangle getRoom() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter length of the room:");
+        double length = sc.nextDouble();
+        System.out.println("Enter width of the room:");
+        double width = sc.nextDouble();
+
+        return new Rectangle(length, width);
+    }
+}
